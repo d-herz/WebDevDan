b551624ca4a4aeb99b026740c860ab7275aa255e
Daniel Hyres <danhyres@gmail.com>
2022-12-19 11:16:25 -0500

    fixed JS event listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,5 @@
 document.querySelector('#exampleEvent1').addEventListener('click', userAlert)
 
-document.querySelector('#exampleEvent2').addEventListener('click', userAlert)
-
 
 function userAlert() {
   alert('This alert brought to you by JavaScript!')
