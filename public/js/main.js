bfed64e52b9bf1d271d1d6f85b1cfaa6ca758592
Daniel Hyres <danhyres@gmail.com>
2022-12-19 10:08:46 -0500

    Fixed blog link formatting

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#exampleEvent1').addEventListener('click', userAlert)
+
+document.querySelector('#exampleEvent2').addEventListener('click', userAlert)
+
+
+function userAlert() {
+  alert('Brought to you by JavaScript!')
+}
