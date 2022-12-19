d8af4539ed588b88fb9b0e5847cba62eca696477
Daniel Hyres <danhyres@gmail.com>
2022-12-19 11:30:45 -0500

    updated script function output

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,5 @@ document.querySelector('#exampleEvent1').addEventListener('click', userAlert)
 
 
 function userAlert() {
-  alert('Yo! This alert brought to you by JavaScript! :)')
+  alert('Yo! This alert brought to you by JavaScript! :D')
 }
